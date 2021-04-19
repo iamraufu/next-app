fd4339d2c1207b4d5c7c8761b9be66bd0deb0dae
Eftykhar Rahman <eftykharraufu@gmail.com>
2021-04-20 02:21:12 +0600

    final commit

---
+++ pages/services/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const index = () => {
+      return (
+            <div>
+                  <h1>Services</h1>
+            </div>
+      );
+};
+
+export default index;
---
+++ pages/services/repair.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const repair = () => {
+      return (
+            <div>
+                  <h1>I repair your broken heart</h1>
+            </div>
+      );
+};
+
+export default repair;
